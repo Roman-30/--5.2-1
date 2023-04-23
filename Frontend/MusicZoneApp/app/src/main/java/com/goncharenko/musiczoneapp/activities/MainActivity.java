7dd13cdf717f91e06e11098deb3052956db05cc3
MaximilianProrock <92297927+MaximilianProrock@users.noreply.github.com>
2023-04-23 19:57:26 +0300

    adding actions with music

--- Frontend/MusicZoneApp/app/src/main/java/com/goncharenko/musiczoneapp/activities/MainActivity.java
+++ Frontend/MusicZoneApp/app/src/main/java/com/goncharenko/musiczoneapp/activities/MainActivity.java
@@ -1,13 +1,18 @@
 package com.goncharenko.musiczoneapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import com.goncharenko.musiczoneapp.R;
 import com.goncharenko.musiczoneapp.fragments.AccountFragment;
@@ -40,6 +45,11 @@ public class MainActivity extends AppCompatActivity implements MainListener{
 //            Fragment fragment = (Fragment) extras.get("firstFragment");
 //            this.firstFragment = fragment;
         }
+
+        if(!checkPermission()){
+            requestPermission();
+            return;
+        }
     }
 
     public void goHome(View view){
@@ -70,5 +80,21 @@ public class MainActivity extends AppCompatActivity implements MainListener{
     @Override
     public void onSignedIn(boolean isSignedIn) {
         this.isSignIn = isSignedIn;
+    }
+
+    boolean checkPermission(){
+        int result = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        if(result == PackageManager.PERMISSION_GRANTED){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    void requestPermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,Manifest.permission.READ_EXTERNAL_STORAGE)){
+            Toast.makeText(MainActivity.this,"READ PERMISSION IS REQUIRED,PLEASE ALLOW FROM SETTTINGS",Toast.LENGTH_SHORT).show();
+        }else
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},123);
     }
 }
--- Frontend/MusicZoneApp/app/src/main/java/com/goncharenko/musiczoneapp/fragments/SearchMusicFragment.java
+++ Frontend/MusicZoneApp/app/src/main/java/com/goncharenko/musiczoneapp/fragments/SearchMusicFragment.java
@@ -1,35 +1,49 @@
 package com.goncharenko.musiczoneapp.fragments;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.os.Bundle;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.goncharenko.musiczoneapp.R;
+import com.goncharenko.musiczoneapp.activities.MainActivity;
+import com.goncharenko.musiczoneapp.models.AudioModel;
+
+import java.io.File;
+import java.util.ArrayList;
 
 public class SearchMusicFragment extends Fragment {
 
-    private String[] music = new String[]{"music_1", "music_2", "music_3", "music_4", "music_5",
-            "music_1", "music_2", "music_3", "music_4", "music_5",
-            "music_1", "music_2", "music_3", "music_4", "music_5"};
-    private ListView listView;
+    private RecyclerView recyclerView;
 
     private ImageButton searchButton;
+
+    ArrayList<AudioModel> songsList = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search_music, container, false);
 
-        listView = view.findViewById(R.id.list_music);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), R.layout.music_item, R.id.music_name, music);
-        listView.setAdapter(adapter);
+        recyclerView = view.findViewById(R.id.recycler_view);
 
         searchButton = view.findViewById(R.id.search_button);
         searchButton.setOnClickListener(new View.OnClickListener() {
@@ -38,9 +52,36 @@ public class SearchMusicFragment extends Fragment {
                 searchMusic(v);
             }
         });
+
+
+        String[] projection = {
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.DATA,
+                MediaStore.Audio.Media.DURATION
+        };
+
+        String selection = MediaStore.Audio.Media.IS_MUSIC +" != 0";
+
+        Cursor cursor = getContext().getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,projection,selection,null,null);
+        while(cursor.moveToNext()){
+            AudioModel songData = new AudioModel(cursor.getString(1),cursor.getString(0),cursor.getString(2));
+            if(new File(songData.getPath()).exists())
+                songsList.add(songData);
+        }
+
+        if(songsList.size()==0){
+            // обработка если нет музыки
+        }else{
+            recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+            recyclerView.setAdapter(new MusicListAdapter(songsList, getContext().getApplicationContext()));
+        }
+
+
         return view;
     }
 
     public void searchMusic(View view) {
     }
+
+
 }
