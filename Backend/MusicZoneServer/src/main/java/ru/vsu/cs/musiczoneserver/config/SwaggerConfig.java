34a0213178b323e2565134d98c057f138e92d208
Roman-30 <Romses2016@yandex.ru>
2023-05-29 10:07:58 +0300

    add header links

--- Backend/MusicZoneServer/src/main/java/ru/vsu/cs/musiczoneserver/config/SwaggerConfig.java
+++ Backend/MusicZoneServer/src/main/java/ru/vsu/cs/musiczoneserver/config/SwaggerConfig.java
@@ -42,9 +42,13 @@ public class SwaggerConfig {
 
         return new ApiInfo(
                 "Music Zone API Documentation",
-                "Documentation of the functionality of the mobile music application: \"Catalog of musical works recorded in electronic form\".",
+                "Documentation of the functionality of the mobile music application: " +
+                        "\"Catalog of musical works recorded in electronic form\".",
                 "1.0.0",
-                "https://github.com/Roman-30/TP-5.2-1/blob/main/Документация/Technical%20task.pdf",
+                "https://github.com/Roman-30/TP-5.2-1/blob/main/" +
+                        "%D0%94%D0%BE%D0%BA%D1%83%D0%BC%D0%B5%D0%BD%D1%82%D0%B0%D1%86%D0%B8%D1%8F%2F%D0%A2%D0%B5" +
+                        "%D1%85%D0%BD%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%BE%D0%B5%20%D0%B7%D0%B0%D0%B4%D0%B0%D0%BD%" +
+                        "D0%B8%D0%B5.pdf",
                 new Contact("Команда TP_5.2-1", "https://github.com/Roman-30/TP-5.2-1", ""),
                 "",
                 "",
