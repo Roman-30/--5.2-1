aad4e99306ac3e63f3a04bcf50d2710d7ca30fe7
Roman-30 <Romses2016@yandex.ru>
2023-06-05 00:19:48 +0300

    update validation

--- Backend/MusicZoneServer/src/main/java/ru/vsu/cs/musiczoneserver/dto/PersonDto.java
+++ Backend/MusicZoneServer/src/main/java/ru/vsu/cs/musiczoneserver/dto/PersonDto.java
@@ -15,7 +15,7 @@ public class PersonDto {
 
     @NotEmpty
     @Pattern(regexp = "^[_A-Za-z0-9-+]+(.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(.[A-Za-z0-9]+)*(.[A-Za-z]{2,})$",
-            message = "Не соответствует формату email!")
+            message = "Does not match the format email!")
     private String email;
 
     private String password;
