ee572309da27531931c2bab67739507fcdb18e98
MaximilianProrock <92297927+MaximilianProrock@users.noreply.github.com>
2023-06-05 17:35:55 +0300

    Final commit

--- Frontend/MusicZoneApp/app/src/main/java/com/goncharenko/musiczoneapp/fragments/EntryFragment.java
+++ Frontend/MusicZoneApp/app/src/main/java/com/goncharenko/musiczoneapp/fragments/EntryFragment.java
@@ -61,7 +61,6 @@ public class EntryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_entry, container, false);
 
         emailInput = view.findViewById(R.id.email_input);
